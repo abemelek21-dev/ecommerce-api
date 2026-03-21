482d7d05101c8d924927427af958bbde5e0675a6
abefiker <new94abemelek@gmail.com>
2026-03-21 17:08:14 +0300

    prisma configured and schema is prepared

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), UserModule, PrismaModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
